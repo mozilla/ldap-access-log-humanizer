7f9cf21dd3f5f5f91da9e654134ba7a249724b38
Jonathan Claudius <jclaudius@mozilla.com>
2018-10-29 10:23:33 -0400

    Add humanizer logic to leverage building blocks

--- test_connection.py
+++ test_connection.py
@@ -148,3 +148,30 @@ class TestConnection():
         assert connection.op == 1
         assert connection.verb == "RESULT"
         assert connection.verb_details == 'tag=97 err=49 text='
+
+    def test_add_event_log(self):
+        event = {'time': 'Oct 26 12:46:58',
+                 'server': 'ldap.example.com',
+                 'process': 'slapd[11086]',
+                 'conn': '6862452',
+                 'rest': 'fd=34 ACCEPT from IP=192.168.1.1:56822 (IP=0.0.0.0:389)'}
+
+        connection = Connection(1245)
+        connection.add_event(event)
+        assert connection.time == event["time"]
+        assert connection.server == event["server"]
+        assert connection.process == event["process"]
+        assert connection.conn_id == 1245
+        assert connection.fd == 34
+        assert connection.op == ""
+        assert connection.verb == "ACCEPT"
+        assert connection.verb_details == "from IP=192.168.1.1:56822 (IP=0.0.0.0:389)"
+        assert connection.client == "192.168.1.1"
+        assert connection.log() == {'client': '192.168.1.1',
+                                    'conn_id': 1245,
+                                    'fd': 34,
+                                    'op': '',
+                                    'time': 'Oct 26 12:46:58',
+                                    'tls': False,
+                                    'verb': 'ACCEPT',
+                                    'verb_details': 'from IP=192.168.1.1:56822 (IP=0.0.0.0:389)'}
