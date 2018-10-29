6eef7b09d7d1e062650e50677a14d4fb7d3cb160
Jonathan Claudius <jclaudius@mozilla.com>
2018-10-29 10:59:20 -0400

    Actually add humanizer

---
+++ humanizer.py
@@ -0,0 +1,42 @@
+import argparse
+from rawlogparser import RawLogParser
+from connection import Connection
+
+parser = argparse.ArgumentParser(
+    description='Humanize (or flatten) an open-ldap log.')
+parser.add_argument('--file', help='path to open-ldap log file')
+args = parser.parse_args()
+
+# Used for connection tracking
+connections = {}
+
+with open(args.file) as fp:
+    for line in fp:
+        # print(line.rstrip())
+        # print("Active Connections: {}".format(len(connections)))
+
+        event = RawLogParser().parse(line.rstrip())
+
+        if event == None:
+            continue
+
+        # Look to see if we have an existing connection
+        connection = connections.get(event['conn'])
+
+        # If we have a pre-existing connection, just add context
+        if connection:
+            # print("Pre-existing connection: {}".format(str(event['conn'])))
+            connection.add_event(event)
+
+            # If the connection is closed, remove from active connections
+            if connection.verb == "closed":
+                connections.pop(event['conn'])
+
+            print(connection.log())
+        # If it's a new connection, just create one and start tracking
+        else:
+            # print("New connection: {}".format(str(event['conn'])))
+            connection = Connection(event['conn'])
+            connection.add_event(event)
+            connections[event['conn']] = connection
+            print(connection.log())
