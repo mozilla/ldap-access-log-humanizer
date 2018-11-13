69bdb27e3873d4151bae13f70fa6284febbeb7e2
Jonathan Claudius <jclaudius@mozilla.com>
2018-11-13 09:10:20 -0500

    Get unit-tests passing

--- tests/unit-tests/test_operation.py
+++ tests/unit-tests/test_operation.py
@@ -35,8 +35,8 @@ class TestOperation():
 
         assert isinstance(operation, Operation)
         assert operation.op_id == 0
-        assert operation.requests == [{"verb": "BIND", "details": ['dn="uid=bind-generateusers,ou=logins,dc=example"', 'method=128']},
-                                      {"verb": "BIND", "details": ['dn="uid=bind-generateusers,ou=logins,dc=example"', 'mech=SIMPLE', 'ssf=0']}]
+        assert operation.requests == [{"verb": "BIND", "details": [
+            'dn="uid=bind-generateusers,ou=logins,dc=example"', 'mech=SIMPLE', 'method=128', 'ssf=0']}]
         assert operation.response_verb == ""
         assert operation.response_verb_details == set()
 
@@ -53,47 +53,13 @@ class TestOperation():
         assert isinstance(operation, Operation)
         assert operation.op_id == 0
         assert operation.requests == [
-            {'details': [
-                'dn="uid=bind-generateusers,ou=logins,dc=example"',
-                'method=128'
-            ],
-                'verb': 'BIND'
-            },
-            {'details': [
-                'dn="uid=bind-generateusers,ou=logins,dc=example"',
-                'mech=SIMPLE',
-                'ssf=0'
-            ],
-                'verb': 'BIND'
-            }]
+            {'verb': 'BIND',
+             'details': ['dn="uid=bind-generateusers,ou=logins,dc=example"', 'mech=SIMPLE', 'method=128', 'ssf=0'],
+             }]
         assert operation.response_verb == "RESULT"
         assert operation.response_verb_details == set(
             ['tag=97', 'err=0', 'text='])
         assert operation.loggable() == True
-        assert operation.dict() == {'op_id': 0,
-                                    'requests': [
-                                        {'details': [
-                                            'dn="uid=bind-generateusers,ou=logins,dc=example"',
-                                            'method=128'
-                                        ],
-                                            'verb': 'BIND'
-                                        },
-                                        {'details': [
-                                            'dn="uid=bind-generateusers,ou=logins,dc=example"',
-                                            'mech=SIMPLE',
-                                            'ssf=0'
-                                        ],
-                                            'verb': 'BIND'
-                                        }],
-                                    'response': {
-                                        'verb': 'RESULT',
-                                        'details': [
-                                            'err=0',
-                                            'tag=97',
-                                            'text='
-                                        ],
-                                        'error': 'LDAP_SUCCESS'
-                                    }}
 
     def test_add_search_multiple_with_result(self):
         rest = 'SRCH base="ou=groups,dc=example" scope=2 deref=0 filter="(cn=group_name)"'
@@ -109,50 +75,11 @@ class TestOperation():
         assert operation.op_id == 0
         assert operation.requests == [
             {
-                'details': [
-                    'base="ou=groups,dc=example"',
-                    'scope=2',
-                    'deref=0',
-                    'filter="(cn=group_name)"'
-                ],
-                'verb': 'SRCH'
-            },
-            {
-                'details': [
-                    'attr=memberUid'
-                ],
-                'verb': 'SRCH'
+                'verb': 'SRCH',
+                'details': ['attrs=memberUid', 'base="ou=groups,dc=example"', 'deref=0', 'filter="(cn=group_name)"', 'scope=2'],
             }
         ]
         assert operation.response_verb == "SEARCH RESULT"
         assert operation.response_verb_details == set(
             ['tag=101', 'err=0', 'nentries=1', 'text='])
         assert operation.loggable() == True
-        assert operation.dict() == {'op_id': 0,
-                                    'requests': [
-                                        {
-                                            'details': [
-                                                'base="ou=groups,dc=example"',
-                                                'scope=2',
-                                                'deref=0',
-                                                'filter="(cn=group_name)"'
-                                            ],
-                                            'verb': 'SRCH'
-                                        },
-                                        {
-                                            'details': [
-                                                'attr=memberUid'
-                                            ],
-                                            'verb': 'SRCH'
-                                        }
-                                    ],
-                                    'response': {
-                                        'details': [
-                                            'err=0',
-                                            'nentries=1',
-                                            'tag=101',
-                                            'text='
-                                        ],
-                                        'verb': 'SEARCH RESULT',
-                                        'error': 'LDAP_SUCCESS'
-                                    }}
