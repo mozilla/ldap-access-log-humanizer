abf4118275f691f4ae9c02a6891a3b4fedc5f16f
Justin Dow <jdow@mozilla.com>
2019-05-30 13:56:29 -0700

    fixing package name

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
     'author_email': 'jdow@mozilla, jclaudius@mozilla.com',
     'version': "0.0.1",
     'install_requires': [],
-    'packages': ['ldap-access-log-humanizer'],
+    'packages': ['ldap_access_log_humanizer'],
     'scripts': ['humanizer.py'],
     'data_files': [('etc/humanizer', ['humanizer_settings.json.default'])],
     'name': 'humanizer'
