589742b98bcafa23423d0eb313ee0d25bfffbdcc
Jonathan Claudius <jclaudius@mozilla.com>
2019-06-05 10:17:39 -0400

    Fix unit-tests

--- tests/unit-tests/test_rawlogparser.py
+++ tests/unit-tests/test_rawlogparser.py
@@ -2,10 +2,11 @@
 from ldap_access_log_humanizer.raw_log_parser import RawLogParser
 
 
-class TestRawLogParser():
-    def __init__(self):
-        self.args_dict = {'output_mozdef': False, 'output_stdout': True, 'input_type': 'file', 'output_file': False, 'output_syslog': False, 'host': '0.0.0.0', 'daemonize': False, 'input_file_name': None, 'mozdef_url': 'https://127.0.0.1:8443/events', 'noconfig': False, 'output_file_name': 'humanizer.log', 'output_stderr': False, 'config': 'humanizer_settings.json', 'port': '1514'}
+TEST_RAW_LOG_PARSER_ARGS_DICT = {'output_mozdef': False, 'output_stdout': True, 'input_type': 'file', 'output_file': False, 'output_syslog': False, 'host': '0.0.0.0', 'daemonize': False,
+                                 'input_file_name': None, 'mozdef_url': 'https://127.0.0.1:8443/events', 'noconfig': False, 'output_file_name': 'humanizer.log', 'output_stderr': False, 'config': 'humanizer_settings.json', 'port': '1514'}
 
+
+class TestRawLogParser():
     def test_line(self):
         log_line = "Oct 26 12:46:58 ldap.example.com slapd[11086]: conn=6862452 fd=34 ACCEPT from IP=192.168.1.1:56822 (IP=0.0.0.0:389)"
         raw_log_parser = RawLogParser(args_dict)
@@ -19,6 +20,6 @@ class TestRawLogParser():
 
     def test_bad_line(self):
         log_line = "Oct 26 03:25:02 ldap.example.com slapd[11086]: connection_read(26): no connection!"
-        raw_log_parser = RawLogParser(self.args_dict)
+        raw_log_parser = RawLogParser(TEST_RAW_LOG_PARSER_ARGS_DICT)
 
         assert raw_log_parser.parse(log_line) == None
