d96429598efb709e4e5a45b623edcc98960218dc
Jonathan Claudius <jclaudius@mozilla.com>
2019-06-05 12:44:53 -0400

    Fix malformed output_syslog args

--- tests/unit-tests/test_custom_logger.py
+++ tests/unit-tests/test_custom_logger.py
@@ -36,7 +36,7 @@ class TestCustomLogger():
         assert err == "hello world\n"
 
     def test_file_with_no_file(self):
-        args_dict = {'output_mozdef': False, 'output_stdout': False, 'input_type': 'file', 'output_file': True, 'output_sys     log': False, 'host': '0.0.0.0', 'daemonize': False,
+        args_dict = {'output_mozdef': False, 'output_stdout': False, 'input_type': 'file', 'output_file': True, 'output_syslog': False, 'host': '0.0.0.0', 'daemonize': False,
                      'input_file_name': None, 'mozdef_url': 'https://127.0.0.1:8443/events', 'noconfig': False, 'output_file_name': '', 'output_stderr': False, 'config': 'humanizer_settings.json', 'port': '1514'}
         logger = CustomLogger(TEST_CUSTOM_LOGGER_ARGS_DICT)
         with pytest.raises(Exception) as excinfo:
@@ -45,7 +45,7 @@ class TestCustomLogger():
 
     def test_file_with_nonexistant_file(self):
         filename = "test_file_with_nonexistant_file.txt"
-        args_dict = {'output_mozdef': False, 'output_stdout': False, 'input_type': 'file', 'output_file': True, 'output_sys     log': False, 'host': '0.0.0.0', 'daemonize': False, 'input_file_name': None,
+        args_dict = {'output_mozdef': False, 'output_stdout': False, 'input_type': 'file', 'output_file': True, 'output_syslog': False, 'host': '0.0.0.0', 'daemonize': False, 'input_file_name': None,
                      'mozdef_url': 'https://127.0.0.1:8443/events', 'noconfig': False, 'output_file_name': filename, 'output_stderr': False, 'config': 'humanizer_settings.json', 'port': '1514'}
 
         # Clean up to make sure we don't have an existing file
@@ -73,7 +73,7 @@ class TestCustomLogger():
 
     def test_file_with_existant_file(self):
         filename = "test_file_with_existant_file.txt"
-        args_dict = {'output_mozdef': False, 'output_stdout': False, 'input_type': 'file', 'output_file': True, 'output_syslog': False, 'output_sys     log': False, 'host': '0.0.0.0', 'daemonize': False, 'input_file_name': None,
+        args_dict = {'output_mozdef': False, 'output_stdout': False, 'input_type': 'file', 'output_file': True, 'output_syslog': False, 'host': '0.0.0.0', 'daemonize': False, 'input_file_name': None,
                      'mozdef_url': 'https://127.0.0.1:8443/events', 'noconfig': False, 'output_file_name': filename, 'output_stderr': False, 'config': 'humanizer_settings.json', 'port': '1514'}
 
         # Clean up to make sure we don't have an existing file
