fdf76b3e2fbdc9aa0647541424f7deac91777ced
Jonathan Claudius <jclaudius@mozilla.com>
2019-07-08 16:43:27 -0400

    Add bind base class and unit-tests for fodder

---
+++ tests/unit-tests/test_bind.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import re
+from ldap_access_log_humanizer.bind import Bind
+
+
+class TestBind():
+    def test_creation(self):
+        bind = Bind('dn="mail=user@example.com,o=com,dc=example" method=128 version=3')
+        assert isinstance(bind, Bind)
+        # TODO
+        # assert operation.dn == 'mail=user@example.com,o=com,dc=example'
+        # assert operation.o == 'com'
+        # assert operation.dc == "example"
+        assert bind.mail() == "user@example.com"
+        assert bind.version() == 3
+        assert bind.method() == 128
